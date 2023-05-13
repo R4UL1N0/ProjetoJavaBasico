0627b6de75ef14239e3744ec5d83ab305d680d96
unknown <h6sw97@gmail.com>
2023-05-13 19:25:56 -0300

    ading functionalities to Main

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,37 @@
-import builders.StudentsBuilder;
-import entities.Student;
+import controllers.StudentsController;
 
 public class Main {
 
     public static void main(String[] args) {
-        var allStudents = StudentsBuilder.getAllStudents();
+
+        System.out.println("APROVADOS:");
+        StudentsController.showApprovedList();
+
+        System.out.println("REPROVADOS:");
+        StudentsController.showFlunkedList();
+
+        System.out.println("ALUNOS NOTA 10:");
+        StudentsController.showNota10();
+
+
+        StudentsController.showLowestOnes();
+
+        System.out.println("DO MAIOR PARA O MENOR:");
+        StudentsController.showDescending();
+
+
+        System.out.println("BEST THREE:");
+        StudentsController.showBestThree();
+
+        System.out.println("WORST THREE:");
+        StudentsController.showWorstThree();
+
 
         // Agora vamos as atividades
         /*
+        
 
-        1. Recupere da lista os alunos que passaram de ano (nota minima 7.0).
-            - Exiba os dados nesse formato: <código> - <nome> : Média = <nota>
-        2. Recupere da lista os alunos que não passaram de ano.
-            - Exiba os dados nesse formato: <código> - <nome> : Média = <media> (Faltou = <nota_faltante>)
-        3. Traga os alunos que tiraram a nota máxima (nota 10).
-            - Exiba os dados nesse formato: <código> - <nome>
-        4. Traga o aluno que tirou a menor nota, em caso de notas iguais, traga ambos os alunos.
-            - Exiba os dados nesse formato: <código> - <nome> : Nota = <nota>
+        
         5. Faça uma lista com top 3 notas de alunos. Em caso de notas iguais coloque todos na mesma posição.
             - Ex:
                 1º - Fulano : Nota = 10.0;
@@ -26,8 +41,6 @@ public class Main {
             - Exiba os dados nesse formato: <posicao> - <nome> : Nota = <nota>
         6. Faça uma lista com as 3 menores notas de alunos. Em caso de notas iguais coloque todos na mesma posição. Exemplo igual a anterior
             - Exiba os dados nesse formato: <posicao> - <nome> : Nota = <nota>
-        7. Monte a média de todos os alunos e exiba em tela ordenando da maior para a menor nota.
-            - Exiba os dados nesse formato: <posicao> - <código> - <nome> : Média = <nota>
 
          */
     }
